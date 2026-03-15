65c60921e84aee17a50d92d51170706a4547041b
Marc San <sanchezpe.marc@gmail.com>
2026-03-15 17:24:32 +0100

    Introducir filas en la lógica

--- project-library.js
+++ project-library.js
@@ -2,6 +2,10 @@ const myLibrary = [];
 
 function Book(id, title, author, pages, read) 
 {
+    if (!new.target) {
+        throw Error('You need to declare the constructor new.')
+    }
+
     this.id = id;
     this.title = title;
     this.author = author;
@@ -11,7 +15,35 @@ function Book(id, title, author, pages, read)
 
 function addBookToLibrary (title, author, pages, read) {
     let id = crypto.randomUUID();
-    let book = Book(id, title, author, pages);
+    let book = new Book(id, title, author, pages);
     myLibrary.push(book)
 } 
 
+const exampleBook = new Book(
+    'The Hobbit',
+    'J.R Tolkien',
+    '295',
+    'not read yet'
+);
+const bookshelves = document.querySelector('#bookshelves');
+
+function bookshelvesDisplay(bookArray) {
+    for (const book in bookArray) {
+        let bookshelvesRow = document.createElement('tr');
+        for (const [key, value] of Object.entries(book)) {
+            let rowData = document.createElement('td');
+            rowData.textContent = value;
+            bookshelvesRow.appendChild(rowData)
+        }
+        bookshelves.appendChild(bookshelvesRow);
+    }
+};
+
+addBookToLibrary(
+    'The Hobbit',
+    'J.R Tolkien',
+    '29',
+    'not read yet'
+)
+
+bookshelvesDisplay(myLibrary);
