aabefafd0fa164d710b4347f0623cb1ab683f314
Marc San <sanchezpe.marc@gmail.com>
2026-03-21 12:32:53 +0100

    Añadir libro ciclo completo

--- project-library.js
+++ project-library.js
@@ -13,35 +13,57 @@ function Book(id, title, author, pages, read)
     this.read = read;
 };
 
+// Se obtiene el contenido de la form
+
+function getFormContent() {
+    let title = document.querySelector('input#title').value;
+    console.log(`title ${title} type ${typeof(title)}`)
+    let author = document.querySelector('input#author').value;
+    console.log(`author ${author} type ${typeof(author)}`)
+    let pages = document.querySelector('input#pages').value;
+    console.log(`pages ${pages} type ${typeof(pages)}`)
+    let read = document.querySelector('input#read').value;
+    console.log(`read ${read} type ${typeof(read)}`)
+    let bookOutput = [title, author, pages, read];
+    return bookOutput
+};
+
+// Se añade un libro a la librería
+
 function addBookToLibrary (title, author, pages, read) {
     let id = crypto.randomUUID();
     let book = new Book(id, title, author, pages, read);
     myLibrary.push(book)
 } 
+
+// Se muestra el último libro en la librería
+
 const bookshelves = document.querySelector('#bookshelves');
 
-function bookshelvesDisplay(bookArray) {
-    for (const book of bookArray) {
-        let bookshelvesRow = document.createElement('tr');
-        console.log(`Book ${bookArray[book]}`)
-        for (const [key, value] of Object.entries(book)) {
-            console.log(`key ${key}`)
-            console.log(`values ${value}`)
-            let rowData = document.createElement('td');
-            rowData.textContent = value;
-            bookshelvesRow.appendChild(rowData)
-        }
-        bookshelves.appendChild(bookshelvesRow);
+function bookshelvesDisplay(lastBook) {
+    let bookshelvesRow = document.createElement('tr');
+    console.log(`Book ${lastBook}`)
+    for (const [key, value] of Object.entries(lastBook)) {
+        console.log(`key ${key}`)
+        console.log(`values ${value}`)
+        let rowData = document.createElement('td');
+        rowData.textContent = value;
+        bookshelvesRow.appendChild(rowData)
     }
+    bookshelves.appendChild(bookshelvesRow);
 };
 
-addBookToLibrary( 
-    'The Hobbit',
-    'J.R Tolkien',
-    '29',
-    'not read yet'
-)
+// Ciclo completo de añadir hasta mostrar libro
 
-console.log(myLibrary)
+function addNewBook(event) {
+    [title, author, pages, read] = getFormContent();
+    addBookToLibrary(title, author, pages, read)
+    console.log(myLibrary.at(-1))
+    bookshelvesDisplay(myLibrary.at(-1))
+    event.preventDefault();
+}
 
-bookshelvesDisplay(myLibrary);
+// Evento que dispara el ciclo
+
+const submitButton = document.querySelector(".form-container button")
+submitButton.addEventListener("click", addNewBook)
